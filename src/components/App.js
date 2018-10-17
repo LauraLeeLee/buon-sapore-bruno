d6727631d5ca6a8f47548edbf4bad7e8157356c8
Laura Logan <laurardh@aol.com>
2018-10-17 13:15:58 -0400

    adds background image

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '../index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container" role="main">
+      
+      </div>
+    );
+  }
+}
+
+export default App;
