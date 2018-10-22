56a9409aff7d3f685e8f745271d43433304a18a6
Laura Logan <laurardh@aol.com>
2018-10-22 16:43:20 -0400

    corrects typos

--- src/components/MenuInfo.js
+++ src/components/MenuInfo.js
@@ -6,18 +6,17 @@ import NavItems from '../data/nav_items.json';
 
 class MenuInfo extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      recipe: '',
-    }
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     recipe: '',
+  //   }
+  // }
 
   render(){
     return(
       <div>
       <article className="menu_info">
-
         <h2>Beef Recipes</h2>
         <img className="food_image" src={ BeefCat } alt="image of beef"/>
       </article>
