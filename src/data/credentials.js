5bf7b999092b8cb5811c6f34d4111a0c2d3e6ded
Laura Logan <laurardh@aol.com>
2019-02-16 14:55:23 -0500

    deletes credentials.js

--- src/data/credentials.js
+++
@@ -1,8 +0,0 @@
-//Firebase API key and authorization info
-
-export const ApiKey = "AIzaSyC5wDGuZ5SLv6RHt-YIHLZIm9ZCOkaZvTo";
-export const AuthDomain = "buon-sapore-bruno.firebaseapp.com";
-export const DatabaseURL = "https://buon-sapore-bruno.firebaseio.com";
-export const ProjectId = "buon-sapore-bruno";
-export const StorageBucket = "buon-sapore-bruno.appspot.com";
-export const MessagingSenderId = "388222118182";
